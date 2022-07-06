2a0b8570314740205569af5112882d09c7c6300e
Nicolas Romero <desarrollador.romero@gmail.com>
2022-07-05 23:29:19 -0500

    Organización del proyecto y actualizacion readme

--- Script_Prueba.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path="Drivers/chromedriver.exe")
-driver.maximize_window()
-
-driver.get("https://www.netflix.com/co/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fbrowse")
-
-driver.close()
